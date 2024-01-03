bdf0012c739248ee1f4331283d76896cc42599c6
ManuelLG92 <m.godinho@stuart.com>
2024-01-03 10:40:17 +0100

    fix format

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id ("com.diffplug.spotless") version "6.23.3"
-    id ("org.springframework.boot") version "3.2.1"
-    id ("io.spring.dependency-management") version "1.1.4"
-    id ("org.jetbrains.kotlin.jvm") version "1.9.21"
-    id ("org.jetbrains.kotlin.plugin.spring") version "1.9.21"
+    id("com.diffplug.spotless") version "6.23.3"
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.jetbrains.kotlin.jvm") version "1.9.21"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.9.21"
     application
 }
 
@@ -19,23 +19,21 @@ repositories {
 
 dependencies {
 
-    developmentOnly ("org.springframework.boot:spring-boot-devtools")
-    implementation ("org.springframework.boot:spring-boot-starter-web")
-    implementation ("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation ("org.jetbrains.kotlin:kotlin-reflect")
-    implementation ("org.springframework.boot:spring-boot-starter-actuator")
-    implementation ("org.springframework.boot:spring-boot-starter-web")
-    implementation ("org.springframework.boot:spring-boot-starter-validation")
-    testImplementation ("org.springframework.boot:spring-boot-starter-test")
-
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
-tasks.withType< KotlinCompile> {
+tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "21"
     }
 }
-
 
 spotless {
     kotlin {
